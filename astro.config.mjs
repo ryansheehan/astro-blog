b19ff57c00a362669b6d2c0b784c8c6d72bccd9e
Ryan Sheehan <rsheehan@gmail.com>
2024-02-16 23:00:59 -0600

    fixed image handling to use astro image pipelines

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,13 @@ import mdx from '@astrojs/mdx';
 
 import sitemap from '@astrojs/sitemap';
 
+import vercelStatic from '@astrojs/vercel/static';
+
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://example.com',
 	integrations: [mdx(), sitemap()],
+
+	output: 'static',
+	adapter: vercelStatic(),
 });
