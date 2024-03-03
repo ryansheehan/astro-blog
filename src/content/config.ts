8634ab8d2cda6cd9792d362466c3f1f70657a521
Ryan Sheehan <rsheehan@gmail.com>
2024-03-02 22:35:01 -0600

    inc commit, updated layouts, added resume data

--- src/content/config.ts
+++ src/content/config.ts
@@ -14,4 +14,55 @@ const blog = defineCollection({
 	}),
 });
 
-export const collections = { blog };
+const resume = defineCollection({
+	type: 'data',
+	schema: () => z.object({
+		title: z.string(),
+		summary: z.string(),
+
+		contact: z.object({
+			name: z.string(),
+			phone: z.string(),
+			email: z.string().email(),
+			location: z.string(),
+			links: z.array(z.object({
+				name: z.string(),
+				url: z.string().url(),
+			})),			
+		}),
+
+		skills: z.array(z.string()),
+
+		highlights: z.array(z.object({
+			title: z.string(),
+			description: z.string(),
+		})),
+
+		history: z.array(z.object({
+			company: z.string(),
+			positions: z.array(z.object({
+			 	title: z.string(),
+			 	dates: z.object({
+			 		start: z.object({
+			 			month: z.number(),
+			 			year: z.number(),
+			 		}),
+			 		end: z.object({
+			 			month: z.number(),
+			 			year: z.number(),
+			 		}).optional()
+			 	}),
+				responsibilities: z.array(z.string()),
+			}))
+		})),
+
+		education: z.array(z.object({
+			school: z.string(),
+			degree: z.string(),
+			level: z.string(),
+			year: z.number()
+		})),
+	})
+})
+
+export const collections = { blog, resume };
