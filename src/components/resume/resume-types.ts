1bd934468a1297873c04222bdca5a4c47f23af2c
Ryan Sheehan <rsheehan@gmail.com>
2024-05-17 00:32:57 -0500

    refactored resume content into smaller components

--- src/components/resume/resume-types.ts
+++ src/components/resume/resume-types.ts
@@ -19,3 +19,29 @@ export type Company = {
     company: string,
     positions: Position[],
 };
+
+export type Degree = {
+    school: string,
+    degree: string,
+    year: number,
+};
+
+export type SkillList = string[];
+
+export type Skills = {
+    [category: string]: SkillList,
+};
+
+export type ResumeLink = {
+    name: string,
+    display: string,
+    url: string,
+};
+
+export type ContactInfo = {
+    name: string,
+    phone: string,
+    email: string,
+    location: string,
+    links: ResumeLink[],   
+};
